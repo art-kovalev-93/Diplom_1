ad93a572bb0234220d349ebecb7dd39a0fce1ddd
Artem Kovalev <kak-19931993@mail.ru>
2025-02-02 00:06:26 +0300

    тесты для bun burger ingredient

---
+++ tests/test_ingredient_class.py
@@ -0,0 +1,33 @@
+import pytest
+from ingredient import Ingredient
+from ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+from unittest.mock import Mock
+
+
+class TestIngredient:
+    @pytest.fixture
+    def ingredient_sample(self):
+        ingredient = Ingredient(ingredient_type='', name='Сыр', price=0.8)
+        return ingredient
+
+    @pytest.mark.parametrize('type', [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING])
+    def test_type_of_ingredient_true(self, ingredient_sample, type):
+        ingredient_sample.type=type
+        assert ingredient_sample.type == type
+
+    def test_name_of_ingredient_true(self, ingredient_sample):
+        assert ingredient_sample.name == 'Сыр'
+
+    def test_price_of_ingredient_true(self, ingredient_sample):
+        assert ingredient_sample.price == 0.8
+
+    @pytest.mark.parametrize('type', [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING])
+    def test_get_type_of_ingredient_true(self, ingredient_sample, type):
+        ingredient_sample.type = type
+        assert ingredient_sample.get_type() == type
+
+    def test_get_name_of_ingredient_true(self, ingredient_sample):
+        assert ingredient_sample.get_name() == 'Сыр'
+
+    def test_get_price_of_ingredient_true(self, ingredient_sample):
+        assert ingredient_sample.get_price() == 0.8
